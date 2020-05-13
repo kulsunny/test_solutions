572a198ebab49315b60b4b8281415ab47ca1267a
Gaurav Sharma <gauravsharma81189@gmail.com>
2020-05-13 11:52:43 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,21 @@
 
 int main(int argc, const char * argv[])
 {
+    std::vector<std::string> v;
+    v.push_back("23123123123123153453453464565464534241234242342348");
+    v.push_back("128");
+    auto ret = checkDivisibility(v);
+    for (auto s : ret)
+    {
+        cout << s << endl;
+    }
+    
+    std::vector<int> x{1,3,5,5,2,-1,-4,-1,1,3};
+    std::vector<int> y{1,2,2,4,4,2,4,-2,-2,-2};
+    
+    std::cout << minArea(x, y, 2) << std::endl;
+    
+    std::cout << distinctMoves("rrlrlr", 6, 1, 3) << std::endl;
     
     return 0;
 }
